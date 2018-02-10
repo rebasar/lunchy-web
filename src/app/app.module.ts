8bc3b98bffaaa506fc59e325d2de12c615ebbeb1
R. Emre Basar <rebasar+git@rebworks.net>
2018-02-10 22:55:47 +0100

    Add Google Sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,11 @@ import { MatList,
   MatIconModule,
   MatCardModule,
   MatButtonModule,
-  MatChipsModule} from '@angular/material';
+  MatChipsModule,
+  MatProgressSpinnerModule} from '@angular/material';
 
+import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 
 import { AppComponent } from './app.component';
 import { LunchyComponent } from './lunchy/lunchy.component';
@@ -20,6 +23,12 @@ import { LunchPlacesComponent } from './lunch-places/lunch-places.component';
 import { LunchyBackendService } from './lunchy-backend.service';
 import { HttpClientModule } from '@angular/common/http';
 
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('Not telling you :)')
+  }
+]);
 
 @NgModule({
   declarations: [
@@ -39,7 +48,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     MatButtonModule,
     MatChipsModule,
-    HttpClientModule
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    SocialLoginModule.initialize(config)
   ],
   providers: [LunchyBackendService],
   bootstrap: [AppComponent]
--- src/app/lunchy/lunchy.component.ts
+++ src/app/lunchy/lunchy.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'angularx-social-login';
 
 import { LunchPlace } from './lunch_place';
 import { LunchItem } from './lunch';
 import { LunchyBackendService } from '../lunchy-backend.service';
 import { LunchRef } from './lunch_ref';
+import { GoogleLoginProvider } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-lunchy',
@@ -12,14 +15,20 @@ import { LunchRef } from './lunch_ref';
 })
 export class LunchyComponent implements OnInit {
 
+  user: SocialUser;
+  loggedIn: boolean;
   places: LunchPlace[] = [];
   selectedPlace?: LunchPlace;
   lunch: LunchRef = LunchRef.notLoaded();
 
-  constructor(private lunchyBackendService: LunchyBackendService) { }
+  constructor(private lunchyBackendService: LunchyBackendService, private authService: AuthService) { }
 
   ngOnInit() {
     this.getLunchPlaces();
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
   }
 
   getLunchPlaces() {
@@ -33,6 +42,14 @@ export class LunchyComponent implements OnInit {
 
   fetchLunchItems(place: LunchPlace): void {
     this.lunchyBackendService.fetchLunchItems(place).subscribe(lunch => this.lunch = lunch);
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
   }
 
 }
