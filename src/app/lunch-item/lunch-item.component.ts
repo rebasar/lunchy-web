d87c9dd016d5621596156c7ea45eda4ab56ed8ff
R. Emre Basar <rebasar+git@rebworks.net>
2018-02-11 12:51:59 +0100

    Add UI for likes

--- src/app/lunch-item/lunch-item.component.ts
+++ src/app/lunch-item/lunch-item.component.ts
@@ -1,6 +1,41 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LunchItem } from '../lunchy/lunch';
 
+enum Sentiment {
+  GOOD,
+  NEUTRAL,
+  BAD
+}
+
+class SentimentState {
+  sentiment?: Sentiment;
+
+  like(): void {
+    this.sentiment = Sentiment.GOOD;
+  }
+
+  neutral(): void {
+    this.sentiment = Sentiment.NEUTRAL;
+  }
+
+  bad(): void {
+    this.sentiment = Sentiment.BAD;
+  }
+
+  isGood(): boolean {
+    return this.sentiment === Sentiment.GOOD;
+  }
+
+  isNeutral(): boolean {
+    return this.sentiment === Sentiment.NEUTRAL;
+  }
+
+  isBad(): boolean {
+    return this.sentiment === Sentiment.BAD;
+  }
+
+}
+
 @Component({
   selector: 'app-lunch-item',
   templateUrl: './lunch-item.component.html',
@@ -9,10 +44,23 @@ import { LunchItem } from '../lunchy/lunch';
 export class LunchItemComponent implements OnInit {
 
   @Input() item: LunchItem;
+  sentiment: SentimentState = new SentimentState();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  like(): void {
+    this.sentiment.like();
+  }
+
+  neutral(): void {
+    this.sentiment.neutral();
+  }
+
+  bad(): void {
+    this.sentiment.bad();
   }
 
 }
